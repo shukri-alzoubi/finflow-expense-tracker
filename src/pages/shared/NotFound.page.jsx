4d76666a6cef41834d2f4b07a98a6cffa33a7780
shukri-alzoubi <alz.shukri@gmail.com>
2026-06-14 16:20:39 -0500

    build NotFound/404 page

--- src/pages/shared/NotFound.page.jsx
+++ src/pages/shared/NotFound.page.jsx
@@ -1,5 +1,24 @@
-const NotFoundPage = () => {
-    return ( <>404 Page</> );
+import { useAuth } from "../../context/Auth.context";
+
+const NotFound = () => {
+    const { user } = useAuth();
+
+    return (<>
+        <div className="vh-100 d-flex flex-column align-items-center justify-content-center">
+            <div class="error-container">
+                <h1 class="error-code">404</h1>
+                <p class="error-message">
+                    The page you're looking for has been moved or doesn't exist. Let's get you back to your workspace.
+                </p>
+                <div className="fs-5">
+                    {user ?
+                        <a href="/dashboard" class="link-secondary">Back to Dashboard</a> :
+                        <a href="/" id="dynamic-home-btn" class="link-secondary">Back to Home</a>
+                    }
+                </div>
+            </div>
+        </div>
+    </>);
 }
- 
-export default NotFoundPage;
+
+export default NotFound;
